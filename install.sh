96cadc7dd1173405d605ac96803ffd1a86d6c779
Joe Previte <jjprevite@gmail.com>
2021-03-21 18:29:11 -0700

    wip: add function verify purchase

--- install.sh
+++ install.sh
@@ -19,4 +19,31 @@ else
   cmd=''
 fi
 
+# TODOS
+# 1. Write function to verify Flurly payment
+# 1. Write function to download course files
+# 1. Add --dry-run option
+# 1. Write documentation
+# 1. Deploy script
+# 1. Test it out
+# 1. Add page to website
+# 1. Plan next step (making actual tutorial/course)
 
+function verify_purchase {
+  echo "Running verify purchase"
+  ## TODO
+  # 1. argumentize the function. first arg should be the paymentId
+  # 2. Remove hardcoded RESPONSE
+  # Note: fo now, I've hard-coded the response.
+  # RESPONSE=$(curl --silent https://flurly.com/api/verify_redirect/cs_live_a1VHFUz7lYnXOL3PUus13VbktedDQDubwfew8E70EvnS1BTOfNTSUXqO0i)
+  RESPONSE='{"payment_status":"paid"}'
+  # credit for helping me figured this grep string thing out
+  # https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
+  echo "$RESPONSE"
+  if grep -q "paid" <<< "$RESPONSE"; then
+   echo "This person has paid"
+  fi
+
+}
+
+verify_purchase
